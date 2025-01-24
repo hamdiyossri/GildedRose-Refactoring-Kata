4e2d4d7b4e66b26dca3a6a618a3bb38b9b4325db
yossri <yosri.hamdi@cruxsoft.be>
2025-01-24 10:22:05 +0100

    refactor: remove unnecessary instruction

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -57,7 +57,7 @@ class GildedRose {
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
